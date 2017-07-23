ed47397c12f0d1fa5aac4e5a45ba17339dc61b32
Kayaba <kayaba@yourfunworld.com>
2017-07-23 17:53:07 -0400

    Update deployment.properties Create a safemode ability (Just in case)

--- client/desktop/src/main/java/com/threerings/bang/client/BangDesktop.java
+++ client/desktop/src/main/java/com/threerings/bang/client/BangDesktop.java
@@ -6,9 +6,12 @@ package com.threerings.bang.client;
 import com.badlogic.gdx.backends.lwjgl.*;
 import com.threerings.bang.steam.*;
 
+import java.io.*;
+
 public class BangDesktop
 {
     public static void main (String[] args) {
+
         System.out.println("Running Bang! Howdy Steam");
         SteamStorage.init();
         System.out.println("Your Steam ID is: " + SteamStorage.user.getSteamID().toString());
@@ -18,6 +21,13 @@ public class BangDesktop
         cfg.height = BangPrefs.getDisplayHeight();
         cfg.depth = BangPrefs.getDisplayBPP();
         cfg.fullscreen = BangPrefs.isFullscreenSet();
+        if(new File("safemode.txt").exists())
+        {
+            cfg.width = 800;
+            cfg.height = 600;
+            cfg.depth = BangPrefs.getDisplayBPP();
+            cfg.fullscreen = false;
+        }
         cfg.resizable = false; // This glitches the game when resized if not set.
         // TODO: cfg.setFromDisplayMode when in fullscreen mode
         new LwjglApplication(new BangApp(), cfg);

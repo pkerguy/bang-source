aa15a596f0c93681894e999c7c4e6e88d8459dca
Kayaba <brandonfairing@yourfunworld.com>
2017-08-04 13:08:13 -0400

    Create obfuscation

--- RunEditor.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -Xmx6G -Xms6G -Djava.library.path=natives -Dresource_dir=rsrc -cp "buildlibs/*" com.threerings.bang.editor.EditorDesktop
-pause
---
+++ buildfinal.sh
@@ -0,0 +1,4 @@
+rm -rf obj/*
+rmdir obj
+mkdir obj
+java -Xms6G -Xmx16G -jar allatori.jar config.xml

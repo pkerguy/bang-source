c746a9cc1bd286890acdbdb7f1c69783b1c25c9a
Kayaba <brandonfairing@yourfunworld.com>
2017-08-04 13:31:44 -0400

    Make things work with Obfuscation.. We only really need these two files obfuscated.. Nothing else.

--- buildfinal.sh
+++ buildfinal.sh
@@ -7,3 +7,6 @@ mkdir obj
 cp -R build/client/. obj/
 rm obj/desktop-1.1-SNAPSHOT.jar
 java -Xms6G -Xmx16G -jar allatori.jar config.xml
+cp -R obj/. final/
+rm final/desktop-1.1-SNAPSHOT.jar
+rm final/shared-1.1-SNAPSHOT.jar

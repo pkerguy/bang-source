3746be1a5d58f5b69471cb9a7a46e6a029d0af33
Kayaba <brandonfairing@yourfunworld.com>
2017-08-04 13:21:27 -0400

    Fixes obfuscation! Messed up some bits

--- buildfinal.sh
+++ buildfinal.sh
@@ -4,5 +4,6 @@ mkdir final
 rm -rf obj/*
 rmdir obj
 mkdir obj
-cp -R build/client/*.jar obj/desktop-1.1-SNAPSHOT.jar
+cp -R build/client/. obj/
+rm obj/desktop-1.1-SNAPSHOT.jar
 java -Xms6G -Xmx16G -jar allatori.jar config.xml

3e2bd4f93dee8e65a67abbf76e266267178d1a79
Brandon Fairing <brandonfairing@yourfunworld.com>
2018-05-26 18:59:14 -0400

    Publish new SteamCMD scripts

--- steam/publish.sh
+++ steam/publish.sh
@@ -1,1 +1,1 @@
-sh builder_linux/steamcmd.sh +login yourfunworldstudiosbuildci sVgj3r6MvfFn5IL +run_app_build_http "../scripts/app_build_1000.vdf" +quit
+sh steamcmd.sh +login yourfunworldstudiosbuildci sVgj3r6MvfFn5IL +run_app_build_http "../scripts/app_build_1000.vdf" +quit

c919fb3bf43aae853cde82d8a2f1cd239078026a
Brandon Fairing <brandonfairing@yourfunworld.com>
2018-05-26 18:38:54 -0400

    Attempting to skip the steamcmd.sh and go directly to just steamcmd

--- steam/publish.sh
+++ steam/publish.sh
@@ -1,1 +1,1 @@
-./builder_linux/steamcmd.sh +login yourfunworldstudiosbuildci sVgj3r6MvfFn5IL +run_app_build_http "../scripts/app_build_1000.vdf" +quit
+./builder_linux/linx32/steamcmd +login yourfunworldstudiosbuildci sVgj3r6MvfFn5IL +run_app_build_http "../scripts/app_build_1000.vdf" +quit

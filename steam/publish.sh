b20108c5ea582d56c329d3a491b796d24ec6353c
Brandon Fairing <brandonfairing@yourfunworld.com>
2018-05-27 13:36:41 -0400

    Move to new SteamSDK CI build account

--- steam/publish.sh
+++ steam/publish.sh
@@ -1,4 +1,4 @@
 echo "Injecting path variables...."
 export PATH=$PATH:/opt/steamcmd
 echo "Done"
-steamcmd.sh +login yourfunworldstudiosbuildci sVgj3r6MvfFn5IL +run_app_build_http "../scripts/app_build_1000.vdf" +quit
+steamcmd.sh +login yourfunworldstudioci sVgj3r6MvfFn5IL +run_app_build_http "../scripts/app_build_1000.vdf" +quit

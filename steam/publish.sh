f22fa2218cacf36425adc4165ec2140f3c74d3ac
Brandon Fairing <brandonfairing@yourfunworld.com>
2018-05-26 21:01:01 -0400

    Move to a completely CUSTOM docker image.. Will this make you happy Steam

--- steam/publish.sh
+++ steam/publish.sh
@@ -1,4 +1,1 @@
-chmod -R +x builder
-chmod -R +x builder_linux
-chmod -R +x builder_osx
-./builder_linux/steamcmd.sh +login yourfunworldstudiosbuildci sVgj3r6MvfFn5IL +run_app_build_http "../scripts/app_build_1000.vdf" +quit
+steamcmd.sh +login yourfunworldstudiosbuildci sVgj3r6MvfFn5IL +run_app_build_http "../scripts/app_build_1000.vdf" +quit
